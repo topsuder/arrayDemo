ef27d840c11048d9e9e5769e8e9a3ad048514eb0
topsuder <suders@vip.qq.com>
2023-04-10 18:12:16 +0800

    init commit

--- src/main/java/fun/suder/ArrayTest.java
+++ src/main/java/fun/suder/ArrayTest.java
@@ -1,7 +1,13 @@
+package fun.suder;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.ArrayUtil;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -16,71 +22,56 @@ import java.util.Set;
  * @version v1.0.0
  * @DATE 2023/4/10-17:18
  * @Description
- * @see PACKAGE_NAME arrayDemo
  */
 public class ArrayTest {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("a,b,c");
-        list.add("b,d");
-        list.add("b,e,f");
-        list.add("g,h");
-        final List<String> merge = merge(list);
-        System.out.println(merge);
+        String[][] arrays =
+                {{"a", "b", "c"}
+                        , {"b", "d"}
+                        , {"b", "g", "h"}
+                        , {"y", "p"}
+                        , {"s", "z"}
+                        , {"z", "q"}};
+        //转为List<List<String>>
+        List<List<String>> list = new ArrayList<>();
+        for (String[] array : arrays) {
+            list.add(Arrays.asList(array));
+        }
 
+        List<List<String>> mergeList = new ArrayList<>();
+
+        for (List<String> array : list) {
+            List<String> jl = array;
+
+            boolean stopFlag = false;
+            for (List<String> strings : mergeList) {
+                if (CollUtil.isNotEmpty(CollUtil.intersection(strings, jl))){
+                    stopFlag = true;
+                    break;
+
+                }
+            }
+            if (stopFlag){
+                continue;
+            }
+
+
+            for (List<String> brray : list) {
+
+
+                boolean flag = false;
+                for (String s : brray) {
+                    if (CollUtil.contains(jl, s)) {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag){
+                    jl = (List<String>) CollUtil.union(jl, brray);
+                }
+            }
+            mergeList.add(jl);
+        }
+        System.out.println(mergeList);
     }
-    public static List<String> merge(List<String> list) {
-        Map<String, Set<String>> map = new HashMap<>();
-        for (String s : list) {
-            String[] elements = s.split(",");
-            for (String element : elements) {
-                Set<String> set = map.computeIfAbsent(element, k -> new HashSet<>());
-                set.addAll(Arrays.asList(elements));
-            }
-        }
-
-        Map<String, Set<String>> merged = new HashMap<>();
-        for (Map.Entry<String, Set<String>> entry : map.entrySet()) {
-            String key = entry.getKey();
-            Set<String> value = entry.getValue();
-
-            if (value.size() > 1) {
-                Set<String> mergedSet = new HashSet<>();
-                for (String s : value) {
-                    mergedSet.addAll(map.get(s));
-                }
-                merged.put(key, mergedSet);
-            }
-        }
-
-        List<String> result = new ArrayList<>();
-        Set<String> visited = new HashSet<>();
-        for (String s : list) {
-            String[] elements = s.split(",");
-            Set<String> intersection = new HashSet<>();
-            for (String element : elements) {
-                Set<String> set = merged.get(element);
-                if (set != null) {
-                    intersection.addAll(set);
-                }
-            }
-            intersection.retainAll(new HashSet<>(Arrays.asList(elements)));
-
-            if (!intersection.isEmpty()) {
-                List<String> mergeList = new ArrayList<>(intersection);
-                mergeList.removeAll(visited);
-                if (!mergeList.isEmpty()) {
-                    visited.addAll(mergeList);
-                    mergeList.add(s.replace(",", ""));
-                    result.add(String.join(",", mergeList));
-                }
-            } else if (!visited.contains(s)) {
-                visited.add(s);
-                result.add(s);
-            }
-        }
-
-        return result;
-    }
-
 }
